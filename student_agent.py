414cf0d3eec9277b6625fcf4c8ac9f915a5a02fb
Yi Fan Li <yifanli123@gmail.com>
2022-11-21 12:52:29 -0500

    Update student_agent.py
    First agent, based on euclidian distance to the opponent player. Win rate of 63%

--- student_agent.py
+++ student_agent.py
@@ -2,6 +2,9 @@
 from agents.agent import Agent
 from store import register_agent
 import sys
+from copy import deepcopy
+import random
+import math
 
 
 @register_agent("student_agent")
@@ -21,7 +24,63 @@ class StudentAgent(Agent):
             "l": 3,
         }
 
+        self.move_map = {
+            "up": (-1,0),
+            "right": (0,1),
+            "down": (1,0),
+            "left": (0,-1),
+        }
+
+        self.autoplay = True
+
     def step(self, chess_board, my_pos, adv_pos, max_step):
+
+
+        s_x, s_y = my_pos
+        e_x, e_y = adv_pos
+
+        moving_agent = deepcopy(my_pos)
+        step_left = max_step
+
+        d_x = e_x - s_x
+        d_y = e_y - s_y
+
+
+        if (abs(d_x) >= step_left):
+            move_x = int(math.copysign(step_left,d_x))
+            step_left = step_left - abs(move_x)
+        
+        elif (abs(d_x) < step_left):
+            move_x = d_x
+            step_left = step_left - abs(move_x)
+        
+        if (abs(d_y) >= step_left):
+            move_y = int(math.copysign(step_left,d_y))
+            step_left = step_left - abs(move_y)
+
+        elif (abs(d_y) < step_left):
+            move_y = d_y
+            step_left = step_left - abs(d_y)
+        
+        my_pos = s_x + move_x, s_y + move_y
+
+        if (e_x == my_pos[0] + 1 and e_y == my_pos[1]):
+            barrier = self.dir_map["d"]
+        
+        elif (e_x == my_pos[0] - 1 and e_y == my_pos[1]):
+            barrier = self.dir_map["u"]
+
+        elif (e_x == my_pos[0] and e_y == my_pos[1] + 1):
+            barrier = self.dir_map["r"]
+        
+        elif (e_x == my_pos[0] and e_y == my_pos[1] - 1):
+            barrier = self.dir_map["l"]
+        
+        else:
+            barrier = random.randint(0, 4)
+        
+        
+
         """
         Implement the step function of your agent here.
         You can use the following variables to access the chess board:
@@ -37,4 +96,4 @@ class StudentAgent(Agent):
         Please check the sample implementation in agents/random_agent.py or agents/human_agent.py for more details.
         """
         # dummy return
-        return my_pos, self.dir_map["u"]
+        return my_pos, barrier
